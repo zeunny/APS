82935ffc1194c97ebe74b5b3071d783954cf9828
이지은 <ee2e@naver.com>
2021-05-17 09:33:51 +0900

    210517 | BinarySearch

---
+++ Algorithm/BinarySearch.py
@@ -0,0 +1,30 @@
+# 1.
+def BinarySearch(Array, left, right, x):
+  if left > right:
+    return -1
+  
+  mid = (left + right) // 2
+  if Array[mid] == x:
+    return mid
+
+  if Array[mid] < x:
+    BinarySearch(Array, left, mid-1, x)
+  else:
+    BinarySearch(Array, mid+1, right, x)
+
+
+# 2.
+def BinarySearch(Array, x):
+  left, right = 0, len(Array)-1
+  while left <= right:
+    mid = (left+right)//2
+
+    if Array[mid] == x:
+      return mid
+
+    if Array[mid] < x:
+      right = mid-1
+    else:
+      left = mid+1
+  
+  return -1
