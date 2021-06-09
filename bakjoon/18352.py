e4616f83bdb5550dfd18629cd1728ce6dbc0008f
이지은 <ee2e@naver.com>
2021-06-09 23:42:05 +0900

    210609 | 다익스트라(인접 행렬)

--- bakjoon/18352.py
+++ bakjoon/18352.py
@@ -1,20 +1,46 @@
-def Floyd(G, n):
-  inf = float('inf')
-  D = [[inf]*(n+1) for _ in range(n+1)]
+# def Floyd(G, n):
+#   inf = float('inf')
+#   D = [[inf]*(n+1) for _ in range(n+1)]
 
-  for i in range(1, n+1):
-    D[i][i] = 0
+#   for i in range(1, n+1):
+#     D[i][i] = 0
 
-  for s, e, c in G:
-    D[s][e] = c
+#   for s, e, c in G:
+#     D[s][e] = c
 
-  for k in range(1, n+1):
-    for i in range(1, n+1):
-      for j in range(1, n+1):
-        if D[i][j] > D[i][k] + D[k][j]:
-          D[i][j] = D[i][k] + D[k][j]
+#   for k in range(1, n+1):
+#     for i in range(1, n+1):
+#       for j in range(1, n+1):
+#         if D[i][j] > D[i][k] + D[k][j]:
+#           D[i][j] = D[i][k] + D[k][j]
 
-  return D
+#   return D
+
+# def Diikstra(V, E):
+#   INF = float('inf')
+#   graph = {i: [] for i in range(V+1)}
+
+#   for s, e, c in E:
+#     graph[s].append([e, c])
+  
+#   dist = [INF]*(V+1)
+#   selected = [False]*(V+1)
+
+#   dist[1] = 0
+#   cnt = 0
+#   while cnt < V:
+#     min_index, min_value = 0, INF
+#     for i, v in enumerate(dist):
+#       if v < min_value and not selected[i]:
+#         min_index = i
+#         min_value = v
+    
+#     selected[min_index] = True
+#     cnt += 1
+#     for i, c in graph[min_index]:
+#       dist[i] = min(dist[i], dist[min_index]+c)
+
+#   return dist
 
 import sys
 
@@ -22,21 +48,32 @@ input = sys.stdin.readline
 
 N, M, K, X = map(int,input().split())
 
-G = []
+INF = float('inf')
+graph = {i: [] for i in range(N+1)}
 
 for _ in range(M):
   s, e = map(int,input().split())
-  G.append((s, e, 1))
+  graph[s].append([e, 1])
 
-D = Floyd(G, N)
+dist = [INF]*(N+1)
+selected = [False]*(N+1)
+
+dist[1] = 0
+cnt = 0
+while cnt < N:
+  min_index, min_value = 0, INF
+  for i, v in enumerate(dist):
+    if v < min_value and not selected[i]:
+      min_index = i
+      min_value = v
+  
+  selected[min_index] = True
+  cnt += 1
+  for i, c in graph[min_index]:
+    dist[i] = min(dist[i], dist[min_index]+c)
 
 answer = []
 
-for i, v in enumerate(D[X]):
+for i, v in enumerate(dist):
   if v == K:
-    answer.append(i)
-
-if answer:
-  print(*answer, sep="\n")
-else:
-  print(-1)
+    print(i)
